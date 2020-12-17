45b3eb39ecc41634d7b3c05c9e06dcfda83324c5
Marisa Vitale <marisa.vitale2306@gmail.com>
2020-12-17 18:15:24 +0100

    cambios

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -40,8 +40,8 @@ const Home = props => {
             <Col md={6}>
 
               <h2 className="mb-3">About us</h2>
-              <p>We Are Freedemy, an on-line learning platform. We help organizations of all kinds prepare for the ever-evolving future of work. </p>
-              <p>Connecting millions of students to the skills they need to succeed.</p>
+              <p>We Are Freedemy, an on-line learning platform. We help organizations of all kinds prepare for the ever-evolving future of work.</p>
+              <p>Connecting millions of students to the skills they need to succeed. We offer the opportunity to open access to education, especially for those whose opportunities have historically been limited. To do this, we’ve partnered with a number of organizations.</p>
 
             </Col>
           </Row>
